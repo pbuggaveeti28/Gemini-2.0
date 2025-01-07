c037e3e5c742fdae4b8e88cee0c6ec9c4827c7c2
Padmaja <pbuggaveeti2811@gmail.com>
2025-01-06 22:38:19 -0800

    Created sidebar

---
+++ Gemini/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    </>
+  )
+}
+
+export default App
